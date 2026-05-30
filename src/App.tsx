1c70745df9bf08b4380a9befc057839665e269c0
Parallel <parallelkim12@gmail.com>
2026-05-30 20:56:30 +0900

    fix: remove BrowserRouter from App to avoid duplicate Router in prerender

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import TopNav from './components/TopNav'
 import Home from './pages/Home'
 import FestivalDetail from './pages/FestivalDetail'
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
     <HelmetProvider>
-      <BrowserRouter>
-        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-          <TopNav />
-          <main style={{ flexGrow: 1 }}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/festival/:id" element={<FestivalDetail />} />
-            </Routes>
-          </main>
-        </div>
-      </BrowserRouter>
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <TopNav />
+        <main style={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/festival/:id" element={<FestivalDetail />} />
+          </Routes>
+        </main>
+      </div>
     </HelmetProvider>
   )
 }
